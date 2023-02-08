79ef83964df10e3d935824f59d782613ec0d8c2f
samiarja <sami.arja@gmail.com>
2023-02-08 13:07:08 +1100

    updated example.py

--- example.py
+++ example.py
@@ -1,43 +1,30 @@
 import event_warping
-import pathlib
+import scipy.io as sio
+import numpy as np
 
-dirname = pathlib.Path(__file__).resolve().parent
-
-(dirname / "figures").mkdir(exist_ok=True)
-
-width, height, events = event_warping.read_es_or_h5_file(
-    dirname / "recordings" / "20220124_201028_Panama_2022-01-24_20~12~11_NADIR"
+events = event_warping.read_es_file(
+    "./recordings/20220124_201028_Panama_2022-01-24_20~12~11_NADIR.h5"
 )
 
-filtered_events = event_warping.without_most_active_pixels(events, ratio=0.01)
+filtered_events = event_warping.without_most_active_pixels(events, ratio=0.001)
+
+Vx = (2.0808552534865946e-05)*1e6
+Vy = (-1.4256183976137506e-06)*1e6
+warped_events = event_warping.warp(
+    filtered_events,
+    velocity=(Vx / 1e6, Vy / 1e6),  # px/µs
+)
 
 cumulative_map = event_warping.accumulate(
     sensor_size=(240, 180),
     events=filtered_events,
-    velocity=(21.49 / 1e6, -0.74 / 1e6),  # px/µs
+    velocity=(Vx / 1e6, Vy / 1e6),  # px/µs
 )
 
-# see https://matplotlib.org/stable/gallery/color/colormap_reference.html
-# for a list of available colormaps
-colormap_name = "magma"
-
-image = event_warping.render(
-    cumulative_map,
-    colormap_name=colormap_name,
-    gamma=lambda image: image,
-)
-image.save(str(dirname / "figures" / "cumulative.png"))
-
-image_pow2 = event_warping.render(
-    cumulative_map,
-    colormap_name=colormap_name,
-    gamma=lambda image: image ** (1 / 2),
-)
-image_pow2.save(str(dirname / "figures" / "cumulative_pow2.png"))
-
-image_pow4 = event_warping.render(
-    cumulative_map,
-    colormap_name=colormap_name,
-    gamma=lambda image: image ** (1 / 4),
-)
-image_pow4.save(str(dirname / "figures" / "cumulative_pow4.png"))
+for ratio in range(1, 5):
+    image = event_warping.render(
+        cumulative_map,
+        colormap_name="magma",  # https://matplotlib.org/stable/gallery/color/colormap_reference.html
+        gamma=lambda image: image ** (1 / ratio),
+    )
+    image.save("./recordings/Panama_r" + str(ratio))
