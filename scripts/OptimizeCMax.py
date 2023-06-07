5574590e889d8256233f2cb4cbddbab091d2483f
samiarja <sami.arja@gmail.com>
2023-06-08 01:22:27 +1000

    rename and organise OptimizeCMax.py

--- scripts/OptimizeCMax.py
+++ scripts/OptimizeCMax.py
@@ -1,60 +1,77 @@
-import concurrent.futures
-import event_warping
-import itertools
-import json
 import numpy as np
-import pathlib
-import sys
-import matplotlib.pyplot as plt
-import PIL.Image
-import scipy.optimize
+import event_warping
+from typing import Tuple
 
-'''
-python3 -m pip install -e . && python scripts/space.py
-'''
+class OptimizeCMax:
 
-VIDEOS          = ["20220124_201028_Panama_2022-01-24_20_12_11_NADIR.h5"]
-OBJECTIVE       = ["variance","weighted_variance","max"]
-method          = ["SLSQP", "BFGS", "Nelder-Mead"]
-SOLVER          = method[1]
-HEURISTIC       = OBJECTIVE[1]
-FILENAME        = VIDEOS[0]
-VELOCITY_RANGE  = (-30, 30)
-RESOLUTION      = 20
-TMAX            = 5e6
-RATIO           = 0.0000001
-READFROM        = "/media/sam/Samsung_T52/PhD/Code/orbital_localisation/data/es/NADIR/"
-SAVEFILETO      = "/media/sam/Samsung_T52/PhD/Code/orbital_localisation/img/"
+    def __init__(self, video_file, objective, solver, velocity_range, resolution, tmax, ratio, read_from, save_to):
+        self.filename = video_file
+        self.heuristic = objective
+        self.solver = solver
+        self.velocity_range = velocity_range
+        self.resolution = resolution
+        self.tmax = tmax
+        self.ratio = ratio
+        self.read_from = read_from
+        self.save_to = save_to
+        self.events = None
+        self.width = None
+        self.height = None
+        self._read_and_process_events()
 
-np.seterr(divide='ignore', invalid='ignore')
-width, height, events = event_warping.read_es_file(READFROM + FILENAME + ".es")
-events = event_warping.without_most_active_pixels(events, ratio=0.000001)
-ii = np.where(np.logical_and(events["t"]>1, events["t"]<(TMAX)))
-events = events[ii]
-print(f"{len(events)=}")
+    def _read_and_process_events(self):
+        np.seterr(divide='ignore', invalid='ignore')
+        self.width, self.height, self.events = event_warping.read_es_file(self.read_from + self.filename + ".es")
+        self.events = event_warping.without_most_active_pixels(self.events, ratio=self.ratio)
+        valid_indices = np.where(np.logical_and(self.events["t"] > 1, self.events["t"] < self.tmax))
+        self.events = self.events[valid_indices]
+        print(f"{len(self.events)=}")
 
-def calculate_heuristic(velocity: tuple[float, float]):
-    if HEURISTIC == "variance":
-        return event_warping.intensity_variance((width, height), events, velocity)
-    if HEURISTIC == "weighted_variance":
-        return event_warping.intensity_weighted_variance((width, height), events, velocity)
-    if HEURISTIC == "max":
-        return event_warping.intensity_maximum((width, height), events, velocity)
-    raise Exception("unknown heuristic")
+    def calculate_heuristic(self, velocity: Tuple[float, float]):
+        if self.heuristic == "variance":
+            return event_warping.intensity_variance((self.width, self.height), self.events, velocity)
+        if self.heuristic == "weighted_variance":
+            return event_warping.intensity_weighted_variance((self.width, self.height), self.events, velocity)
+        if self.heuristic == "max":
+            return event_warping.intensity_maximum((self.width, self.height), self.events, velocity)
+        raise Exception("unknown heuristic")
 
-def callback(velocity: np.ndarray):
-    print(f"velocity={velocity * 1e3}")
+    def callback(self, velocity: np.ndarray):
+        print(f"velocity={velocity * 1e3}")
 
-initial_v = (20 / 1e6, 1 / 1e6) #(21.49 / 1e6, -0.74 / 1e6)
-STEP = events["t"][-1] #500000
-ii = np.where(np.logical_and(events["t"]>1, events["t"]<(STEP)))
+    def optimize(self, initial_velocity):
+        batch = self.events["t"][-1]
+        valid_indices = np.where(np.logical_and(self.events["t"] > 1, self.events["t"] < batch))
+        optimized_velocity = event_warping.optimize_local(sensor_size=(self.width, self.height),
+                                                          events=self.events[valid_indices],
+                                                          initial_velocity=initial_velocity,
+                                                          heuristic_name=self.heuristic,
+                                                          method=self.solver,
+                                                          callback=self.callback)
+        print(optimized_velocity)
+        return optimized_velocity
 
-optimized_velocity = event_warping.optimize_local(sensor_size=(width, height),
-                                                events=events[ii],
-                                                initial_velocity=initial_v,
-                                                heuristic_name=OBJECTIVE[1],
-                                                method=SOLVER,
-                                                callback=callback)
 
-speed = [optimized_velocity[0],optimized_velocity[1]]
-print(optimized_velocity)
+if __name__ == "__main__":
+    video_file          = ["20220124_201028_Panama_2022-01-24_20_12_11_NADIR.h5"]
+    objective           = ["variance","weighted_variance","max"]
+    solver              = ["SLSQP", "BFGS", "Nelder-Mead"]
+    velocity_range      = (-30, 30)
+    resolution          = 20
+    tmax                = 10e6
+    ratio               = 0.0000001
+    read_from           = "/home/samiarja/Desktop/PhD/Code/orbital_localisation/data/es/NADIR/"
+    save_to             = "/home/samiarja/Desktop/PhD/Code/orbital_localisation/img/"
+    initial_velocity    = (20 / 1e6, -1 / 1e6)
+
+    event_processor = OptimizeCMax(video_file[0], 
+                                   objective[1], 
+                                   solver[-1], 
+                                   velocity_range, 
+                                   resolution, 
+                                   tmax, 
+                                   ratio, 
+                                   read_from, 
+                                   save_to)
+    
+    event_processor.optimize(initial_velocity)
